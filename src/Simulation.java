7fbc4f9423f299df2bf02d9b877d0a6ea4fc9855
Braeden Brettin <braeden.lee.brettin@gmail.com>
2017-01-27 14:34:27 -0500

    added more functions
    need to repopulate and mutate. then run everything again

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Simulation
@@ -8,6 +9,7 @@ public class Simulation
 	static double maxTemp;
 	static double minPrec;
 	static double maxPrec;
+	static ArrayList<Plant> population;
 	
 	public static void main(String[] args)
 	{	
@@ -16,6 +18,30 @@ public class Simulation
 		
 		// Set the initial acceptable range for both temperature and precipitation.
 		setInitialRange();
+		
+		// Create a new population of Plants.
+		population = new ArrayList<Plant>();
+		
+		// Add 20 Plants to population.
+		populate();
+		
+		// Select all plants that lie within the ideal ranges for both temperature and precipitation.
+		population = selectBestFit();
+		
+		System.out.println(population.size());
+		
+		// Create children by pairing the remaining Plants from population.
+		ArrayList<Plant> children = crossPollinate();
+		
+		System.out.println(children.size());
+		
+		// Carry over as many parents as possible from population.
+		carryOver(children);
+		
+		// population should now become children.
+		population = children;
+		
+		System.out.println(population.size());
 	}
 	
 	// Accept user input to set the ideal temperature and precipitation.
@@ -51,6 +77,66 @@ public class Simulation
 		{
 			minPrec = idealPrec - (100.0 - idealPrec) / 2.0;
 			maxPrec = 100.0 - (100.0 - idealPrec) / 2.0;
+		}
+	}
+
+	// Add enough plants to population so the population size is 20.
+	public static void populate()
+	{
+		while(population.size() < 20)
+		{
+			double temp = -10 + 50 * Math.random();
+			double prec = 0 + 100 * Math.random();
+			Plant plant = new Plant(temp, prec);
+			population.add(plant);
+		}
+	}
+	
+	// Return an arrayList of all the plants that lie within the ideal ranges for both temperature and precipitation.
+	public static ArrayList<Plant> selectBestFit()
+	{
+		ArrayList<Plant> bestFit = new ArrayList<Plant>();
+		
+		for(int i=0; i<population.size(); i++)
+		{
+			if(population.get(i).getTemp() >= minTemp &&
+				population.get(i).getTemp() <= maxTemp &&
+				population.get(i).getPrecipitation() >= minPrec &&
+				population.get(i).getPrecipitation() <= maxPrec)
+			{
+				bestFit.add(population.get(i));
+			}
+		}
+		
+		return bestFit;
+	}
+	
+	// Create children by pairing the remaining Plants from population.
+	public static ArrayList<Plant> crossPollinate()
+	{
+		ArrayList<Plant> children = new ArrayList<Plant>();
+		
+		for(int i=0; i<population.size(); i=i+2)
+		{
+			if(i != population.size() - 1)
+			{
+				Plant child1 = new Plant(population.get(i).getTemp(), population.get(i+1).getPrecipitation());
+				Plant child2 = new Plant(population.get(i+1).getTemp(), population.get(i).getPrecipitation());
+				children.add(child2);
+				children.add(child1);
+			}			
+		}
+		
+		return children;
+	}
+	
+	// Carry over as many parents as possible from population.
+	public static void carryOver(ArrayList<Plant> children)
+	{		
+		while(children.size() < 20 && population.size() > 0)
+		{
+			children.add(population.get(0));
+			population.remove(0);
 		}
 	}
 }
--- src/SimulationTest.java
+++ src/SimulationTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.*;
+import java.util.ArrayList;
 import org.junit.Test;
 
 public class SimulationTest
@@ -6,14 +7,36 @@ public class SimulationTest
 	@Test
 	public void test()
 	{
-		Simulation.main(null);
+		Simulation.setConditions();
 		
 		assertTrue(Simulation.idealTemp == 15.0);
 		assertTrue(Simulation.idealPrec == 50.0);
+		
+		Simulation.setInitialRange();
 		
 		assertTrue(Simulation.minTemp == 2.5);
 		assertTrue(Simulation.maxTemp == 27.5);
 		assertTrue(Simulation.minPrec == 25.0);
 		assertTrue(Simulation.maxPrec == 75.0);
+		
+		Simulation.population = new ArrayList<Plant>();
+		
+		Simulation.populate();
+		
+		assertTrue(Simulation.population.size() == 20);
+		
+		Simulation.population = Simulation.selectBestFit();
+		
+		assertTrue(Simulation.population.size() <= 20);
+		
+		ArrayList<Plant> children = Simulation.crossPollinate();
+		
+		assertTrue(children.size() == Simulation.population.size() || children.size() == Simulation.population.size() - 1);
+		
+		Simulation.carryOver(children);
+		
+		Simulation.population = children;
+		
+		assertTrue(Simulation.population.size() <= 20);
 	}
 }
